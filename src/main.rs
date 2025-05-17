9627152deff5765242c3db10bb53a43fb4f10569
Delton Ding <dsh0416@gmail.com>
2025-05-18 00:14:45 +0800

    fix window size and rtv

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::max, process::exit};
+use std::{cmp::max, ffi::c_void, process::exit, sync::Mutex};
 
 use windows::{
     core::{BOOL, PCSTR},
@@ -6,68 +6,95 @@ use windows::{
         Foundation::{HMODULE, HWND, LPARAM, LRESULT, RECT, WPARAM},
         Graphics::{Direct3D::*, Direct3D11::*, Dxgi::{Common::*, *}, Gdi::*},
         Storage::Xps::*,
-        System::LibraryLoader::GetModuleHandleW,
+        System::LibraryLoader::GetModuleHandleA,
         UI::WindowsAndMessaging::*
     },
 };
 
+static RTV: Mutex<Option<ID3D11RenderTargetView>> = Mutex::new(None);
+
 fn main() {
-    // Get capture window rect once to get the size
-    let mut rect_lyrics: RECT = RECT::default();
+    // Get capture window rect once to get thea size
     let hwnd_capture_lyrics = find_lyrics_window();
-    _ = unsafe { GetWindowRect(hwnd_capture_lyrics, &mut rect_lyrics) };
-    let width_lyrics = rect_lyrics.right - rect_lyrics.left;
-    let height_lyrics = rect_lyrics.bottom - rect_lyrics.top;
-    println!("Lyrics Window found: {:?}", hwnd_capture_lyrics);
-    println!("Lyrics Window size: {}x{}", width_lyrics, height_lyrics);
+    let (mut width_lyrics, mut height_lyrics) = get_window_size(hwnd_capture_lyrics);
+    println!("Lyrics Window found: {:?}, size: {}x{}", hwnd_capture_lyrics, width_lyrics, height_lyrics);
 
     // Get score window rect once to get the size
-    let mut rect_score: RECT = RECT::default();
     let hwnd_capture_score = find_score_window();
-    _ = unsafe { GetWindowRect(hwnd_capture_score, &mut rect_score) };
-    let width_score = rect_score.right - rect_score.left;
-    let height_score = rect_score.bottom - rect_score.top;
-    println!("Score Window found: {:?}", hwnd_capture_score);
-    println!("Score Window size: {}x{}", width_score, height_score);
+    let (mut width_score, mut height_score) = get_window_size(hwnd_capture_score);
+    println!("Score Window found: {:?}, size: {}x{}", hwnd_capture_score, width_score, height_score);
 
-    let hwnd = init_window(width_lyrics, height_lyrics, width_score, height_score);
+    let (width, height) = calc_window_size(width_lyrics, height_lyrics, width_score, height_score);
+    let hwnd = init_window(width, height);
 
-    let (device, device_context, swapchain) = init_d3d11(hwnd, max(width_lyrics, width_score), height_lyrics + height_score);
+    let (device, device_context, swapchain) = init_d3d11(hwnd);
 
-    let frame_buffer = unsafe { swapchain.GetBuffer::<ID3D11Texture2D>(0).expect("Failed to get buffer") };
-
-    let mut rtv: Option<ID3D11RenderTargetView> = None;
-
-    // Create a render target view
-    unsafe { device.CreateRenderTargetView(&frame_buffer, None, Some(&mut rtv)).expect("Failed to create render target view") };
-    unsafe { device_context.OMSetRenderTargets(Some(&[rtv.clone()]), None) };
-
-    // clear
-    let clear_color = [0.0, 0.0, 0.0, 1.0];
-    unsafe { device_context.ClearRenderTargetView(rtv.as_ref().unwrap(), &clear_color) };
+    // Clear the RTV
+    {
+        let frame_buffer = unsafe { swapchain.GetBuffer::<ID3D11Texture2D>(0).expect("Failed to get buffer") };
+        let mut rtv: Option<ID3D11RenderTargetView> = None;
+        unsafe { device.CreateRenderTargetView(&frame_buffer, None, Some(&mut rtv)).expect("Failed to create render target view") };
+        unsafe { device_context.OMSetRenderTargets(Some(&[rtv.clone()]), None) };
+        RTV.lock().unwrap().replace(rtv.clone().unwrap());
+    }
 
     loop {
         // Handle messages using PeekMessageA
-        let mut msg = MSG::default();
-        while unsafe { PeekMessageA(&mut msg, Some(hwnd), 0, 0, PM_REMOVE) }.0 != 0 {
-            unsafe {
+        unsafe {
+            let mut msg = MSG::default();
+            while PeekMessageA(&mut msg, Some(hwnd), 0, 0, PM_REMOVE).0 != 0 {
                 _ = TranslateMessage(&msg);
                 DispatchMessageA(&msg);
             }
         }
 
+        // Check if the window is resized
+        {
+            let (new_width_lyrics, new_height_lyrics) = get_window_size(hwnd_capture_lyrics);
+            let (new_width_score, new_height_score) = get_window_size(hwnd_capture_score);
+            if new_width_lyrics != width_lyrics || new_height_lyrics != height_lyrics ||
+                new_width_score != width_score || new_height_score != height_score {
+                width_lyrics = new_width_lyrics;
+                height_lyrics = new_height_lyrics;
+                width_score = new_width_score;
+                height_score = new_height_score;
+
+                println!("Lyrics Window resized: {}x{}", width_lyrics, height_lyrics);
+                println!("Score Window resized: {}x{}", width_score, height_score);
+
+                // Resize the window
+                let mut rect: RECT = RECT::default();
+                _ = unsafe { GetClientRect(hwnd, &mut rect) };
+                let (width, height) = calc_window_size(width_lyrics, height_lyrics, width_score, height_score);
+                _ = unsafe { SetWindowPos(hwnd, None, 0, 0, width, height, SWP_NOZORDER | SWP_NOMOVE) };
+
+                // Resize the swap chain
+                { *RTV.lock().unwrap() = None; }
+                unsafe { device_context.OMSetRenderTargets(None, None) };
+                unsafe { swapchain.ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, DXGI_SWAP_CHAIN_FLAG(0)) }.expect("Failed to resize swap chain");
+                let frame_buffer = unsafe { swapchain.GetBuffer::<ID3D11Texture2D>(0).expect("Failed to get buffer") };
+
+                let mut rtv: Option<ID3D11RenderTargetView> = None;             
+                unsafe { device.CreateRenderTargetView(&frame_buffer, None, Some(&mut rtv)).expect("Failed to create render target view") };
+                unsafe { device_context.OMSetRenderTargets(Some(&[rtv.clone()]), None) };
+                RTV.lock().unwrap().replace(rtv.clone().unwrap());
+            }
+        }
+
         // Render the frame
-        unsafe {
-            // copy
+        {
+            let frame_buffer = unsafe { swapchain.GetBuffer::<ID3D11Texture2D>(0).expect("Failed to get buffer") };
+
+            // copya
             let texture_score = capture_image_score(hwnd_capture_score, width_score, height_score, device.clone());
             let texture_lyrics = capture_image_lyrics(hwnd_capture_lyrics, width_lyrics, height_lyrics, device.clone());
             if texture_lyrics.is_some() {
-                device_context.CopySubresourceRegion(&frame_buffer, 0, 0, 0, 0,  &texture_lyrics.unwrap(), 0, None);
+                unsafe { device_context.CopySubresourceRegion(&frame_buffer, 0, 0, 0, 0,  &texture_lyrics.unwrap(), 0, None) };
             }
-            device_context.CopySubresourceRegion(&frame_buffer, 0, 0, height_lyrics.try_into().unwrap(), 0, &texture_score, 0, None);
+            unsafe { device_context.CopySubresourceRegion(&frame_buffer, 0, 0, height_lyrics.try_into().unwrap(), 0, &texture_score, 0, None) };
 
             // Present the frame
-            let res = swapchain.Present(1, DXGI_PRESENT(0));
+            let res = unsafe { swapchain.Present(1, DXGI_PRESENT(0)) };
             if res.is_err() {
                 println!("Failed to present frame: {:?}", res);
             }
@@ -75,24 +102,28 @@ fn main() {
     }
 }
 
-fn init_window(lyrics_width: i32, lyrics_height: i32, score_width: i32, score_height: i32) -> HWND {
+fn calc_window_size(lyrics_width: i32, lyrics_height: i32, score_width: i32, score_height: i32) -> (i32, i32) {
+    let caption_height = unsafe { GetSystemMetrics(SM_CYFRAME) + GetSystemMetrics(SM_CYCAPTION) + GetSystemMetrics(SM_CXPADDEDBORDER) };
+    let border_thickness_x = unsafe { GetSystemMetrics(SM_CXFRAME) };
+    let border_thickness_y = unsafe { GetSystemMetrics(SM_CYFRAME ) };
+    let padding_x = unsafe { GetSystemMetrics(SM_CXPADDEDBORDER) };
+    let width = max(lyrics_width, score_width) + border_thickness_x * 2 + padding_x;
+    let height = lyrics_height + score_height + border_thickness_y * 2 + caption_height;
+
+    (width, height)
+}
+
+fn init_window(width: i32, height: i32) -> HWND {
     let wc = WNDCLASSEXA {
         cbSize: std::mem::size_of::<WNDCLASSEXA>() as u32,
         style: CS_CLASSDC,
         lpfnWndProc: Some(wnd_proc),
-        hInstance: unsafe { GetModuleHandleW(None).unwrap().into() },
+        hInstance: unsafe { GetModuleHandleA(None).unwrap().into() },
         lpszClassName: PCSTR("KG Capture\0".as_ptr()),
         ..Default::default()
     };
 
     unsafe { RegisterClassExA(&wc) };
-
-    let caption_height = unsafe { GetSystemMetrics(SM_CYFRAME) + GetSystemMetrics(SM_CYCAPTION) + GetSystemMetrics(SM_CXPADDEDBORDER) };
-    let border_thickness_x = unsafe { GetSystemMetrics(SM_CXFRAME) };
-    let border_thickness_y = unsafe { GetSystemMetrics(SM_CYFRAME ) };
-    let padding_x = unsafe { GetSystemMetrics(SM_CXPADDEDBORDER) };
-    let window_width = max(lyrics_width, score_width) + border_thickness_x * 2 + padding_x;
-    let window_height = lyrics_height + score_height + border_thickness_y * 2 + caption_height;
 
     let hwnd = unsafe { CreateWindowExA(
         WINDOW_EX_STYLE(0),
@@ -101,8 +132,8 @@ fn init_window(lyrics_width: i32, lyrics_height: i32, score_width: i32, score_he
         WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
         100,
         100,
-        window_width,
-        window_height,
+        width,
+        height,
         None,
         None,
         Some(wc.hInstance),
@@ -115,17 +146,17 @@ fn init_window(lyrics_width: i32, lyrics_height: i32, score_width: i32, score_he
     hwnd
 }
 
-fn init_d3d11(hwnd: HWND, width: i32, height: i32) -> (ID3D11Device, ID3D11DeviceContext, IDXGISwapChain) {
+fn init_d3d11(hwnd: HWND) -> (ID3D11Device, ID3D11DeviceContext, IDXGISwapChain) {
     let swap_chain_desc = DXGI_SWAP_CHAIN_DESC {
         BufferDesc: DXGI_MODE_DESC {
-            Width: width as u32,
-            Height: height as u32,
+            Width: 0,
+            Height: 0,
             RefreshRate: DXGI_RATIONAL { Numerator: 60, Denominator: 1 },
             Format: DXGI_FORMAT_B8G8R8A8_UNORM,
             ..Default::default()
         },
         BufferUsage: DXGI_USAGE_RENDER_TARGET_OUTPUT,
-        BufferCount: 2,
+        BufferCount: 1,
         OutputWindow: hwnd,
         Windowed: BOOL(1),
         SampleDesc: DXGI_SAMPLE_DESC { Count: 1, Quality: 0 },
@@ -171,36 +202,54 @@ fn init_d3d11(hwnd: HWND, width: i32, height: i32) -> (ID3D11Device, ID3D11Devic
     (d3d11_device.unwrap(), d3d11_device_context.unwrap(), swap_chain.unwrap())
 }
 
+fn get_window_size(hwnd: HWND) -> (i32, i32) {
+    let mut rect: RECT = RECT::default();
+    _ = unsafe { GetClientRect(hwnd, &mut rect) };
+    let width = rect.right - rect.left;
+    let height = rect.bottom - rect.top;
+    (width, height)
+}
+
 fn capture_image_lyrics(hwnd: HWND, lyrics_width: i32, lyrics_height: i32, device: ID3D11Device) -> Option<ID3D11Texture2D>{
     let window_dc = unsafe { GetDC(Some(hwnd)) };
     let capture_dc = unsafe { CreateCompatibleDC(Some(window_dc)) };
 
-    let bitmap = unsafe { CreateCompatibleBitmap(window_dc, lyrics_width, lyrics_height) };
+    let bitmap_header = BITMAPINFOHEADER {
+        biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
+        biWidth: lyrics_width,
+        biHeight: -lyrics_height, // Negative to indicate a top-down bitmap
+        biPlanes: 1,
+        biBitCount: 32,
+        biCompression: BI_RGB.0,
+        biSizeImage: (lyrics_width * lyrics_height) as u32,
+        biXPelsPerMeter: 0,
+        biYPelsPerMeter: 0,
+        biClrUsed: 0,
+        biClrImportant: 0,
+        ..Default::default()
+    };
+
+    let bitmap_info = BITMAPINFO {
+        bmiHeader: bitmap_header,
+        ..Default::default()
+    };
+
+    // create an empty pointer that will be filled with the bitmap data
+    let mut buffer_ptr: *mut c_void = std::ptr::null_mut();
+    let bitmap = unsafe { CreateDIBSection(Some(window_dc), &bitmap_info, DIB_RGB_COLORS, &mut buffer_ptr, None, 0) }.unwrap();
     _ = unsafe { SelectObject(capture_dc, bitmap.into()) };
-
-    // 全民K歌 set WS_EX_LAYERED, which causes the GDI or DXGI to not capture the window correctly
-    // Fortunately, PrintWindow with the PRINT_WINDOW_FLAGS(2) flag works
-    _ = unsafe { PrintWindow(hwnd, capture_dc, PRINT_WINDOW_FLAGS(PW_RENDERFULLCONTENT))}; 
-
-    // TODO: maybe try using Windows Graphics Capture API to avoid flickering?
 
     let mut found = true;
 
-    // try at most 100 times to get the bitmap
-    for _ in 1..100 {
-        // 全民K歌 would try to cover the window with white after receiving WM_PAINT message, considering vertical sync, white may not fill every row.
-        // Here we only check the first pixel of each row, if any of them is white, it means the window is covered, ignore this bitmap
-        // otherwise, break the loop
-        for i in 0..lyrics_height {
-            let pixel_row = unsafe { GetPixel(capture_dc, 0, i) }.0;
-            let r = (pixel_row & 0xFF) as u8;
-            if r == 0xFF {
-                found = false;
-                break;
-            }
-        }
+    _ = unsafe { PrintWindow(hwnd, capture_dc, PRINT_WINDOW_FLAGS(PW_RENDERFULLCONTENT))}; 
 
-        if found { break; }
+    for i in 0..lyrics_height {
+        let pixel_row = unsafe { GetPixel(capture_dc, 0, i) }.0;
+        let r = (pixel_row & 0xFF) as u8;
+        if r == 0xFF {
+            found = false;
+            break;
+        }
     }
 
     if !found {
@@ -226,46 +275,11 @@ fn capture_image_lyrics(hwnd: HWND, lyrics_width: i32, lyrics_height: i32, devic
         MiscFlags: 0,
     };
 
-    let mut buffer_data = vec![0u8; (lyrics_width * lyrics_height * 4) as usize];
+
 
     // create bitmap from the HBITMAP
-    let bitmap_header = BITMAPINFOHEADER {
-        biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
-        biWidth: lyrics_width,
-        biHeight: -lyrics_height, // Negative to indicate a top-down bitmap
-        biPlanes: 1,
-        biBitCount: 32,
-        biCompression: BI_RGB.0,
-        biSizeImage: 0,
-        biXPelsPerMeter: 0,
-        biYPelsPerMeter: 0,
-        biClrUsed: 0,
-        biClrImportant: 0,
-        ..Default::default()
-    };
-
-    let mut bitmap_info = BITMAPINFO {
-        bmiHeader: bitmap_header,
-        ..Default::default()
-    };
-
-    let result = unsafe {
-        GetDIBits(
-            capture_dc,
-            bitmap,
-            0,
-            lyrics_height as u32,
-            Some(buffer_data.as_mut_ptr() as *mut _),
-            &mut bitmap_info as *mut _ as *mut BITMAPINFO,
-            DIB_RGB_COLORS
-        )
-    };
-    if result == 0 {
-        panic!("Failed to get DIB bits");
-    }
-
     let texture_data = D3D11_SUBRESOURCE_DATA {
-        pSysMem: buffer_data.as_ptr() as *const _,
+        pSysMem: buffer_ptr,
         SysMemPitch: (lyrics_width * 4) as u32,
         SysMemSlicePitch: (lyrics_width * lyrics_height * 4) as u32,
     };
@@ -444,3 +458,4 @@ unsafe extern "system" fn wnd_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam:
         }
     }
 }
+
